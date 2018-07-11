cdbe588dba0545f6fcbfc366362558bbe424b492
mohammad.jumaa <codepassion89@gmail.com>
2018-07-11 17:48:37 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+//import { robots } from './robots';
+import './App.css';
 
 class  App extends React.Component {
     constructor(){
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }   
     }
-    onSearchChange(event){
-        console.log();
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=> response.json())
+        .then(users => 
+            this.setState({robots:users})
+        );
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+       
     }
     render(){
+         const filterRobots= this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+         if(this.state.robots.length>0)
         return (
     <div className='tc'>
-    <h1 className='tc '>SKYLINE TEAM</h1>
+        <h1 className='f1'>SKYLINE TEAM</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots}/>
-    </div>
+        <CardList robots={filterRobots}/>
+    </div> 
     )
+    else{
+        return <h1 className='tc'>Loading...</h1>;
+    }
     }
 
 }
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ const Card = (data) =>{
     const  data1 =data.data;
     return(
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src={`https://robohash.org/${data1.id}?200x200`} alt='{data1.name}' />
+        <img src={`https://robohash.org/${data1.name}?200x200`} alt='{data1.name}' />
         <div>
             <h2>{data1.name}</h2>
         <p>{data1.Title}</p>
