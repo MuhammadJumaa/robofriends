babff14d6d59f0c1633c1c571bad26e29f55b288
Muhammad Jumaa <codepassion89@gmail.com>
2018-07-27 00:22:48 -0800

    add new features

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,9 +26,47 @@ class  App extends React.Component {
     }
     render(){
         const{robots,searchfield}=this.state;
-         const filterRobots= robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+         const filterRobots= 
+         robots.filter(robot => {
+            var searchResult= robot.name.toLowerCase().includes(searchfield.toLowerCase()); 
+
+            return searchResult;
+        })      
+
+if(filterRobots.length===0){
+     return !robots.length ?  <h1 className='tc'>Loading...</h1> :( 
+    <div className='tc'>
+        <h1 className='f1'>SKYLINE TEAM</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+        <CardList robots={[
+       
+       {
+        id:1,
+        name:searchfield,
+        linkname:searchfield,
+        Title:searchfield
+        },{
+        id:1,
+        name:searchfield+' '+searchfield,
+        linkname:searchfield+' '+searchfield,
+        Title:searchfield+' '+searchfield
+        },{
+        id:1,
+        name:searchfield+' '+searchfield+' '+searchfield,
+        linkname:searchfield+' '+searchfield+' '+searchfield,
+        Title:searchfield+' '+searchfield+' '+searchfield
+        },{
+        id:1,
+        name:searchfield+' '+searchfield+' '+searchfield+' '+searchfield,
+        linkname:searchfield+' '+searchfield+' '+searchfield+' '+searchfield,
+        Title:searchfield+' '+searchfield+' '+searchfield+' '+searchfield
+        },
+    ]}/>
+        </Scroll>
+    </div> 
+    ) ;
+}
         return !robots.length ?  <h1 className='tc'>Loading...</h1> :( 
     <div className='tc'>
         <h1 className='f1'>SKYLINE TEAM</h1>
