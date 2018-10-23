9cfe8f7c66e6937fbb0a151bf799bd9b6803789c
mohammad.jumaa <codepassion89@gmail.com>
2018-10-23 12:02:17 +0300

    fix suggistion search

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,10 +6,10 @@ import Scroll from '../components/scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 //import { robots } from './robots';
 import '../containers/App.css';
-import {connect} from 'react-redux';
-import { setSearchField,requestRobots } from '../actions';
+import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
 
-const mapStateToProps=state=>{
+const mapStateToProps = state => {
     return {
         searchField: state.searchRobots.searchField,
         robots: state.requestRobots.robots,
@@ -17,79 +17,72 @@ const mapStateToProps=state=>{
         error: state.requestRobots.error
     }
 }
-const mapDispatchToProps=(dispatch)=>{
-  return {  
-    onSearchChange:(event)=>dispatch(setSearchField(event.target.value)),
-    onRequestRobots:()=>dispatch(requestRobots())
-  }
-}
-class  App extends React.Component {   
-
-    componentDidMount(){
-       this.props.onRequestRobots();
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
-   
-    render(){
-        const {searchField,onSearchChange,robots,isPending}=this.props
-         const filterRobots= 
-         robots.filter(robot => {
-            var searchResult= robot.name.toLowerCase().includes(searchField.toLowerCase()); 
+}
+class App extends React.Component {
+
+    componentDidMount() {
+        this.props.onRequestRobots();
+    }
+
+    render() {
+        const { searchField, onSearchChange, robots, isPending } = this.props
+        const filterRobots =
+        robots.filter(robot => {
+            var searchResult = robot.name.toLowerCase().includes(searchField.toLowerCase());
 
             return searchResult;
-        })      
+        })
+        var Content = null;
+        if (filterRobots.length === 0&&!isPending) {
+            console.log("empty");
+            Content = 
+            (
+                <CardList robots={[
+                {
+                    id:1,
+                    name:searchField,
+                    linkname:searchField,
+                    Title:searchField
+                    },{
+                    id:1,
+                    name:searchField+' '+searchField,
+                    linkname:searchField+' '+searchField,
+                    Title:searchField+' '+searchField
+                    },{
+                    id:1,
+                    name:searchField+' '+searchField+' '+searchField,
+                    linkname:searchField+' '+searchField+' '+searchField,
+                    Title:searchField+' '+searchField+' '+searchField
+                    },{
+                    id:1,
+                    name:searchField+' '+searchField+' '+searchField+' '+searchField,
+                    linkname:searchField+' '+searchField+' '+searchField+' '+searchField,
+                    Title:searchField+' '+searchField+' '+searchField+' '+searchField
+                },
+                ]}/>
+            );
+        }
+        else
+        Content = isPending ? "Loading...":(<CardList robots={filterRobots}/>);
+        return (
+            <div className='tc'>
+                <Header />
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        {Content}
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+            );
 
-if(filterRobots.length===0){
-     return !robots.length ?  <h1 className='tc'>Loading...</h1> :( 
-    <div className='tc'>
-        <Header />
-        <SearchBox searchChange={onSearchChange} />
-        <ErrorBoundry>
-        <Scroll>
-        <CardList robots={[
-       
-       {
-        id:1,
-        name:searchField,
-        linkname:searchField,
-        Title:searchField
-        },{
-        id:1,
-        name:searchField+' '+searchField,
-        linkname:searchField+' '+searchField,
-        Title:searchField+' '+searchField
-        },{
-        id:1,
-        name:searchField+' '+searchField+' '+searchField,
-        linkname:searchField+' '+searchField+' '+searchField,
-        Title:searchField+' '+searchField+' '+searchField
-        },{
-        id:1,
-        name:searchField+' '+searchField+' '+searchField+' '+searchField,
-        linkname:searchField+' '+searchField+' '+searchField+' '+searchField,
-        Title:searchField+' '+searchField+' '+searchField+' '+searchField
-        },
-    ]}/>
-        </Scroll>
-        </ErrorBoundry>
-    </div> 
-    ) ;
-}
-        return isPending ?  <h1 className='tc'>Loading...</h1> :( 
-    <div className='tc'>
-        <Header />
-        <SearchBox searchChange={onSearchChange} />
-        
-        <Scroll>
-        <ErrorBoundry>
-        <CardList robots={filterRobots}/>
-        </ErrorBoundry>
-        </Scroll>
-        
-    </div> 
-    ) ;
-    
     }
 
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
